01b2dd8653e3cc844f0a73cf51bd940b5e3bdafc
Abdullah al Thalha <abdullahalthalha@gmail.com>
2023-05-03 18:01:27 +0600

    example for primitive data type

---
+++ primitiveDataType.dart
@@ -0,0 +1,13 @@
+void main() {
+//for integer value;
+
+  String studentName = 'Thalha';
+  int studentAge = 21;
+  double studnetCGPA = 3.5;
+  bool isStudnetFromSylhet = true;
+
+  print(studentName);
+  print(studentAge);
+  print(studnetCGPA);
+  print(isStudnetFromSylhet);
+}
