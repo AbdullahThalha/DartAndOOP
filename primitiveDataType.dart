51eb14d9174a92846dd4d3dd1be3065915bcbadb
Abdullah al Thalha <abdullahalthalha@gmail.com>
2023-05-03 22:11:53 +0600

    example for primitive data type

--- primitiveDataType.dart
+++ primitiveDataType.dart
@@ -1,19 +1,37 @@
 void main() {
 
 
-  //primitive data type;
+  //primitive data type:Number(integer,floating point),String,Bool:
 
-  //string datatype;
-  String studentName = 'Thalha';
-  //integer datatype;
+  //Dart number:
+  //integer
   int studentAge = 21;
-  //double datatype;
+  //double
+  double studentId=8;
   double studnetCGPA = 3.5;
+  print(studentAge);
+  print(studentId);
+  print(studnetCGPA);
+
+  //string
+  String singleString = 'A string having single quote';
+  String doubleString="This is a string having double quotes,304/5";
+  String doubleString2="It's also a double "
+      "quote";
+  String tripleString='''This is a "string" having 
+  three quotes''';
+  String concate=singleString+' '+doubleString;
+
+  print(singleString);
+  print(doubleString);
+  print(doubleString2);
+  print(tripleString);
+  print(concate);
+
+
   //boolean datatype;
   bool isStudnetFromSylhet = true;
-
-  print(studentName);
-  print(studentAge);
-  print(studnetCGPA);
+  bool isStudentFromDhaka=false;
   print(isStudnetFromSylhet);
+  print(isStudentFromDhaka);
 }
